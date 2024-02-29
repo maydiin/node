82d6aac7e1e13dae06184ee94c8300e2f725e537
maydiin <maaydiiin@hotmail.com>
2024-03-01 00:25:11 +0300

    express.js

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express= require("express");
 const app= express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
 
 const data= [
-    {id:1, name:"iphone 14", price:3000},
-    {id:2, name:"iphone 15", price:4000},
-    {id:3, name:"iphone 13", price:5000}
+    {id:1, name:"iphone 14", price:3000, imageUrl:"1.jpg"},
+    {id:2, name:"iphone 15", price:4000, imageUrl:"2.jpg"},
+    {id:3, name:"iphone 13", price:5000, imageUrl:"3.jpg"}
 
 ];
 
 // routes
 app.use("/products/:id",function(req,res){
-    res.render("urun_details");
+    const urun = data.find(u => u.id==req.params.id);
+    res.render("urun_details", urun);
 });
 
 app.use("/products",function(req,res){
